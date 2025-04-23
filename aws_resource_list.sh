ff347006b9c29a8e73e2a9a832c946318817ee17
Rom Jr <58005198+RomanchJR@users.noreply.github.com>
2025-04-23 23:10:15 +0545

    Add files via upload

---
+++ aws_resource_list.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###############################################################
+# This script will list all the resouces in the AWS.
+# Author: Romanch Jung Rayamajhi
+# Version: v0.0.1
+# Date: 22nd April, 2025
+# Listed below are the supported AWS services by the script
+# 1. EC2
+# 2. S3
+# 3. DynamoDB
+# 4. Lambda
+# 5. EBS
+# 6. ELB
+# 7. CloudFront
+# 8. CloudWatch
+# 9. SNS
+# 10. SQS
+# 11. Route53
+# 12. VPC
+# 13. RDS
+# 14. IAM
+# 15. CloudFormation
+
+# Usage: ./aws_resource_list.sh <region> <service_name>
+# Example: ./aws_resource_list.sh us-east-1 EC2
+####################################################################
