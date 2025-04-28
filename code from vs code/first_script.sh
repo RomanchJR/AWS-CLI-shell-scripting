1978d4184e2af64ce49ff2060cd3a151cd5329fb
Rom Jr <58005198+RomanchJR@users.noreply.github.com>
2025-04-29 04:06:24 +0545

    Add files via upload

---
+++ code from vs code/first_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author: Romanch Jung Rayamajhi
+# Created: $(date)
+# Purpose: First Shell Script to automate simple tasks
+
+echo "Hello, friends. It's me Romanch!"
+echo "Today's date is: $(date)"
+echo "You are currently in: $(pwd)"
+echo "Here are the files in this directory:"
+ls -la
